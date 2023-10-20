6b45cc5eca71b24f5e7931991e1b37f35bf5973b
Khiet Tam Nguyen <nktnet999@gmail.com>
2023-10-20 18:18:19 +1100

    renamed option tests to path tests

--- tests/path/path.test.ts
+++ tests/path/path.test.ts
@@ -1,7 +1,12 @@
 import importSync from '../../src';
 
-test('ESM import with default options', () => {
+test('ESM import with default relative path', () => {
   const exampleMjs = importSync('../basic/basic');
+  expect(exampleMjs.helloString).toStrictEqual('helloworld');
+});
+
+test('ESM import with absolute path', () => {
+  const exampleMjs = importSync(process.cwd() + '/tests/basic/basic');
   expect(exampleMjs.helloString).toStrictEqual('helloworld');
 });
 
