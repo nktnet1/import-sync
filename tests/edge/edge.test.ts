6722e46164fb310d979bb8d0af7cc8e0e43287d3
Khiet Tam Nguyen <nktnet999@gmail.com>
2023-10-21 14:22:38 +1100

    Tests for extensions [js, mjs, cjs, ts]

--- tests/edge/edge.test.ts
+++ tests/edge/edge.test.ts
@@ -1,9 +1,19 @@
 import importSync from '../../src';
 
 test('Empty file', () => {
-  expect(importSync('./empty')).toStrictEqual({});
+  expect(importSync('./empty')).toMatchObject({});
 });
 
 test('Broken file', () => {
   expect(() => importSync('./broken')).toThrow(Error);
+});
+
+test.each([
+  { extension: '', message: 'JavaScript' },
+  { extension: '.js', message: 'JavaScript' },
+  { extension: '.mjs', message: 'ECMAScript' },
+  { extension: '.cjs', message: 'CommonJS' },
+  { extension: '.ts', message: 'TypeScript' },
+])("Testing extension '$extension' contains message '$message'", ({ extension, message }) => {
+  expect(importSync(`./extension${extension}`)).toMatchObject({ message });
 });
