d9c8bcb0209c9d350bc306b2b14cb3a5a829b510
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-04-20 08:31:10 +1000

    deps: bump package versions + tsdown

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,3 @@ import importSync from './import';
 
 export default importSync;
 export type { Options, ESMOptions } from './options';
-
-module.exports = importSync;
-module.exports.default = importSync;
