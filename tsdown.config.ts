5b0d97e17e52eb48d8add71f540e7a62748bf71a
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-04-20 09:18:24 +1000

    chore: tsdown remove array for format

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: './src/index.ts',
-  format: ['commonjs'],
+  format: 'commonjs',
   platform: 'node',
   outDir: './dist',
   dts: true,
