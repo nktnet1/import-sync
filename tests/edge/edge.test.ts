a4c466e9175bcb72e889c48ac0091d3b53ec162f
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-05-18 10:25:00 +1000

    style(tests): format edge

--- tests/edge/edge.test.ts
+++ tests/edge/edge.test.ts
@@ -9,14 +9,14 @@ test("Broken file", () => {
   expect(() => importSync("./broken")).toThrow(Error);
 });
 
-test('Literal alias', () => {
-  expect(importSync('./literal-alias')).toMatchObject({
+test("Literal alias", () => {
+  expect(importSync("./literal-alias")).toMatchObject({
     myVar: 1531,
     default: 1531,
-    'module.exports': 1531,
+    "module.exports": 1531,
     myVarAlias: 1531,
-    '  __  :)  __  ': 1531,
-    ' 😆 ': 1531,
+    "  __  :)  __  ": 1531,
+    " 😆 ": 1531,
   });
 });
 
