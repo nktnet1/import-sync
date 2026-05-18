20aa2936b5f0c1504ba5899e621e90ca2fd879db
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-05-18 11:02:00 +1000

    chore: rename test files

--- tests/esmlibs/esmlibs.test.ts
+++ tests/esmlibs/esmlibs.test.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "@jest/globals";
 import importSync from "../../src";
 
-test("ESM no extension relativePath", () => {
+test("Esmlib importing node-fetch", () => {
   expect(typeof importSync("node-fetch").default).toStrictEqual("function");
 });
 
-test("ESM no extension relativePath", () => {
+test("Esmlib importing node-datachannel", () => {
   expect(typeof importSync("node-datachannel").initLogger).toStrictEqual(
     "function",
   );
