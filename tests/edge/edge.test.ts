88e91e60c7fcab6cf6d5ae00dc2b5045a0a33fc5
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-05-18 10:29:26 +1000

    chore: rearrange imports

--- tests/edge/edge.test.ts
+++ tests/edge/edge.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@jest/globals";
+import { expect, test } from "@jest/globals";
 import importSync from "../../src";
 
 test("Empty file", () => {
--- tests/path/path.test.ts
+++ tests/path/path.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@jest/globals";
+import { expect, test } from "@jest/globals";
 import importSync from "../../src";
 
 test("ESM import with default relative path", () => {
